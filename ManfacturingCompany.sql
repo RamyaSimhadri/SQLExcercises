dfb98c09e924d55c320a24fab50f22790f89a23f
Ramya Simhadri <ramyasimhadri22@gmail.com>
2016-04-06 12:52:17 -0400

    P2:Program to keep track of the details of a manufacturing company

---
+++ ManfacturingCompany.sql
@@ -0,0 +1,79 @@
+create table employee1
+(
+emp_id int not null, /* EMPLOYEE ID,ACTS AS A PRIMARY COLOUMN*/
+e_name varchar(20), /* EMPLLYEE NAME */
+e_age int not null, /* EMPLOYEE AGE*/
+address varchar(25),
+primary key(emp_id)
+);
+/* TABLE PROJECT */
+create table project 
+(
+project_id int not null,/* ACTS AS A PRIMARY COLUMN */
+project_name varchar(255),
+p_start_date varchar(20), /*PROJECT START DATE */
+p_end_date varchar(20), /* PROJECT END DATE */
+emp_id int not null,
+primary key (project_id),
+foreign key (emp_id) references employee(emp_id)
+);
+
+
+
+/* TABLE SUPPLY*/
+create table supply
+(
+s_id int not null, /* SUPPLIER ID*/
+s_name varchar(20),/* SUPPLIER NAME*/
+s_address varchar(30),/*SUPPLIER ADDRESS*/
+primary key(s_id)
+
+);
+
+/* TABLE WAREHOUSE */
+create table warehouse
+(
+w_id int not null,/* WAREHOUSE ID*/
+w_address varchar(25),/* WAREHOUSE ADDRESS */
+w_capacity int not null,/* WAREHOUSE CAPACITY*/
+primary key(w_id)
+);
+
+create table parts
+(
+part_id int not null,
+part_name varchar(20),
+project_id int not null,
+primary key(part_id),
+s_id int not null,
+w_id int not null,
+foreign key(project_id) references project(project_id),
+foreign key(s_id) references supply(s_id),
+foreign key(w_id) references warehouse(w_id)
+);
+
+/* POPULATING VALUES INTO A TABLE*/
+
+insert into employee1 values('1','ramya','22','4566,oakton road');
+insert into employee1 values('2','ram','28','4567,allen road');
+
+insert into project values ('11','MICROSTRATEGY','03-24-2016','03-24-2017','1');
+insert into project values ('12','RALLY','03-24-2018','03-24-2019','2');
+
+insert into supply values ('67','aishu','3456,breckett terrace');
+
+insert into supply values ('69','amrutha','3446,ghazwa terrace');
+
+insert into warehouse values('123','2346 gandhipuram','45 ');
+insert into warehouse values('234','2313 danvaipeta','34 ');
+
+insert into parts values ('99','RAM','11','67','123');
+insert into parts values ('100','ROM','12','69','234');
+
+select * from project where emp_id='1';/* getting project details by the employee id */
+
+select * from parts where s_id='67';/* getting part details for the given supplier id */
+
+
+
+
